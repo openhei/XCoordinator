4f70ec3048bb69b4291adcb4e8b8963f28a7bfcf
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-12-29 04:36:05 +0100

    Add documentation for NavigationTransition

--- XCoordinator/Classes/NavigationTransition.swift
+++ XCoordinator/Classes/NavigationTransition.swift
@@ -9,8 +9,22 @@
 public typealias NavigationTransition = Transition<UINavigationController>
 
 extension Transition where RootViewController: UINavigationController {
-    public static func push(_ presentable: Presentable, animation: Animation? = nil) -> NavigationTransition {
-        return NavigationTransition(presentables: [presentable], animation: animation?.presentationAnimation) { options, performer, completion in
+
+    ///
+    /// Pushes a presentable on the rootViewController's navigation stack.
+    ///
+    /// - Parameter presentable:
+    ///     The presentable to be pushed onto the navigation stack.
+    ///
+    /// - Parameter animation:
+    ///     The animation to set for the presentable. Its presentationAnimation will be used for the
+    ///     immediate push-transition, its dismissalAnimation is used for the pop-transition,
+    ///     if not otherwise specified. Specify `nil` here to leave animations as they were set for the
+    ///     presentable before. You can use `Animation.default` to reset the previously set animations
+    ///     on this presentable.
+    ///
+    public static func push(_ presentable: Presentable, animation: Animation? = nil) -> Transition {
+        return Transition(presentables: [presentable], animation: animation?.presentationAnimation) { options, performer, completion in
             performer.push(
                 presentable.viewController,
                 with: options,
@@ -23,8 +37,17 @@ extension Transition where RootViewController: UINavigationController {
         }
     }
 
-    public static func pop(animation: Animation? = nil) -> NavigationTransition {
-        return NavigationTransition(presentables: [], animation: animation?.dismissalAnimation) { options, performer, completion in
+    ///
+    /// Pops the topViewController from the rootViewController's navigation stack.
+    ///
+    /// - Parameter animation:
+    ///     The animation to set for the presentable. Only its dismissalAnimation is used for the
+    ///     pop-transition. Specify `nil` here to leave animations as they were set for the
+    ///     presentable before. You can use `Animation.default` to reset the previously set animations
+    ///     on this presentable.
+    ///
+    public static func pop(animation: Animation? = nil) -> Transition {
+        return Transition(presentables: [], animation: animation?.dismissalAnimation) { options, performer, completion in
             performer.pop(
                 toRoot: false,
                 with: options,
@@ -34,8 +57,22 @@ extension Transition where RootViewController: UINavigationController {
         }
     }
 
-    public static func pop(to presentable: Presentable, animation: Animation? = nil) -> NavigationTransition {
-        return NavigationTransition(presentables: [presentable], animation: animation?.dismissalAnimation) { options, performer, completion in
+    ///
+    /// Pops viewControllers from the rootViewController's navigation stack until the specified
+    /// presentable is reached.
+    ///
+    /// - Parameter presentable:
+    ///     The presentable to pop to. Make sure this presentable is in the rootViewController's
+    ///     navigation stack before performing such a transition.
+    ///
+    /// - Parameter animation:
+    ///     The animation to set for the presentable. Only its dismissalAnimation is used for the
+    ///     pop-transition. Specify `nil` here to leave animations as they were set for the
+    ///     presentable before. You can use `Animation.default` to reset the previously set animations
+    ///     on this presentable.
+    ///
+    public static func pop(to presentable: Presentable, animation: Animation? = nil) -> Transition {
+        return Transition(presentables: [presentable], animation: animation?.dismissalAnimation) { options, performer, completion in
             performer.pop(
                 to: presentable.viewController,
                 options: options,
@@ -45,8 +82,18 @@ extension Transition where RootViewController: UINavigationController {
         }
     }
 
-    public static func popToRoot(animation: Animation? = nil) -> NavigationTransition {
-        return NavigationTransition(presentables: [], animation: animation?.dismissalAnimation) { options, performer, completion in
+    ///
+    /// Pops viewControllers from the rootViewController's navigation stack until only one viewController
+    /// is left.
+    ///
+    /// - Parameter animation:
+    ///     The animation to set for the presentable. Only its dismissalAnimation is used for the
+    ///     pop-transition. Specify `nil` here to leave animations as they were set for the
+    ///     presentable before. You can use `Animation.default` to reset the previously set animations
+    ///     on this presentable.
+    ///
+    public static func popToRoot(animation: Animation? = nil) -> Transition {
+        return Transition(presentables: [], animation: animation?.dismissalAnimation) { options, performer, completion in
             performer.pop(
                 toRoot: true,
                 with: options,
@@ -56,8 +103,21 @@ extension Transition where RootViewController: UINavigationController {
         }
     }
 
-    public static func set(_ presentables: [Presentable], animation: Animation? = nil) -> NavigationTransition {
-        return NavigationTransition(presentables: presentables, animation: animation?.presentationAnimation) { options, performer, completion in
+    ///
+    /// Sets the navigation stack of the rootViewController to the specified presentables.
+    ///
+    /// - Parameter presentables:
+    ///     The presentables to make up the navigation stack after the transition is done.
+    ///
+    /// - Parameter animation:
+    ///     The animation to set for the presentable. Its presentationAnimation will be used for the
+    ///     transition animation of the top-most viewController, its dismissalAnimation is used for
+    ///     any pop-transition of the whole navigation stack, if not otherwise specified. Specify `nil`
+    ///     here to leave animations as they were set for the presentables before. You can use
+    ///     `Animation.default` to reset the previously set animations on all presentables.
+    ///
+    public static func set(_ presentables: [Presentable], animation: Animation? = nil) -> Transition {
+        return Transition(presentables: presentables, animation: animation?.presentationAnimation) { options, performer, completion in
             performer.set(
                 presentables.map { $0.viewController },
                 with: options,
