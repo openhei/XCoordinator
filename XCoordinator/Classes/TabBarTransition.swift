38acfba8450caff38a504a63cc8be25c93b98e7a
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2018-12-30 16:22:22 +0100

    Add documentation for TabBarTransition

--- XCoordinator/Classes/TabBarTransition.swift
+++ XCoordinator/Classes/TabBarTransition.swift
@@ -9,8 +9,19 @@
 public typealias TabBarTransition = Transition<UITabBarController>
 
 extension Transition where RootViewController: UITabBarController {
-    public static func set(_ presentables: [Presentable], animation: Animation? = nil) -> TabBarTransition {
-        return TabBarTransition(presentables: presentables, animation: animation?.presentationAnimation) { options, performer, completion in
+
+    ///
+    /// Transition to set the tabs of the rootViewController with an optional custom animation.
+    ///
+    /// - Parameter presentables:
+    ///     The tabs to be set are defined by the presentables' viewControllers.
+    ///
+    /// - Parameter animation:
+    ///     The animation to be used. If you specify `nil` here, the default animation by UIKit is used.
+    ///     Only the presentation animation of the Animation object is used.
+    ///
+    public static func set(_ presentables: [Presentable], animation: Animation? = nil) -> Transition {
+        return Transition(presentables: presentables, animation: animation?.presentationAnimation) { options, performer, completion in
             performer.set(
                 presentables.map { $0.viewController },
                 with: options,
@@ -20,8 +31,19 @@ extension Transition where RootViewController: UITabBarController {
         }
     }
 
-    public static func select(_ presentable: Presentable, animation: Animation? = nil) -> TabBarTransition {
-        return TabBarTransition(presentables: [presentable], animation: animation?.presentationAnimation) { options, performer, completion in
+    ///
+    /// Transition to select a tab with an optional custom animation.
+    ///
+    /// - Parameter presentable:
+    ///     The tab to be selected is the presentable's viewController. Make sure that this is one of the
+    ///     previously specified tabs of the rootViewController.
+    ///
+    /// - Parameter animation:
+    ///     The animation to be used. If you specify `nil` here, the default animation by UIKit is used.
+    ///     Only the presentation animation of the Animation object is used.
+    ///
+    public static func select(_ presentable: Presentable, animation: Animation? = nil) -> Transition {
+        return Transition(presentables: [presentable], animation: animation?.presentationAnimation) { options, performer, completion in
             performer.select(
                 presentable.viewController,
                 with: options,
@@ -31,8 +53,18 @@ extension Transition where RootViewController: UITabBarController {
         }
     }
 
-    public static func select(index: Int, animation: Animation? = nil) -> TabBarTransition {
-        return TabBarTransition(presentables: [], animation: animation?.presentationAnimation) { options, performer, completion in
+    ///
+    /// Transition to select a tab with an optional custom animation.
+    ///
+    /// - Parameter index:
+    ///     The index of the tab to be selected. Make sure that there is a tab at the specified index.
+    ///
+    /// - Parameter animation:
+    ///     The animation to be used. If you specify `nil` here, the default animation by UIKit is used.
+    ///     Only the presentation animation of the Animation object is used.
+    ///
+    public static func select(index: Int, animation: Animation? = nil) -> Transition {
+        return Transition(presentables: [], animation: animation?.presentationAnimation) { options, performer, completion in
             performer.select(
                 index: index,
                 with: options,
